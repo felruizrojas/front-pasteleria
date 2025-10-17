ee04317a76af5d35ced5e036ce8074bd939f7ebd
Felipe Ruiz Rojas <felruizrojas@Felipes-MacBook-Air-2.local>
2025-10-17 01:01:16 -0300

    Updates

---
+++ assets/logo_tienda-CU7G0dT7.js
@@ -0,0 +1,1 @@
+const o="/front-pasteleria/assets/logo_tienda-nq5N5rUZ.png";export{o as l};
---
+++ assets/useAuth-Ce2KblCh.js
@@ -0,0 +1,1 @@
+import{r,i as e}from"./index-CqkjqQ36.js";const s=()=>{const t=r.useContext(e);if(!t)throw new Error("useAuth must be used within an AuthProvider");return t};export{s as u};
